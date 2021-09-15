ebb9f0f9aba3b95a2eaf06de4e6abbd9b3d53d4e
minorut <minorut@users.noreply.github.com>
2021-09-15 21:00:44 +0900

    First commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eux
+
+ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/.config/git/ignore ~/.config/git/ignore
+ln -s $PWD/.config/iterm2/com.googlecode.iterm2.plist .config/iterm2/com.googlecode.iterm2.plist
