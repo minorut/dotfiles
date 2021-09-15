0b5201be4c0620405b6117939792710ea3cfa3ec
minorut <minorut@users.noreply.github.com>
2021-09-15 21:02:05 +0900

    Fix path

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -eux
 
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.config/git/ignore ~/.config/git/ignore
-ln -s $PWD/.config/iterm2/com.googlecode.iterm2.plist .config/iterm2/com.googlecode.iterm2.plist
+ln -s $PWD/.config/iterm2/com.googlecode.iterm2.plist ~/.config/iterm2/com.googlecode.iterm2.plist
